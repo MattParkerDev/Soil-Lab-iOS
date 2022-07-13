063f452264afe7d2ec5d4909ed98262ccb84c157
Matthew Parker <matthewparker@hotmail.com.au>
2022-07-13 18:53:53 +1000

    Initial Commit

---
+++ Soil-Lab/Soil_LabApp.swift
@@ -0,0 +1,17 @@
+//
+//  Soil_LabApp.swift
+//  Soil-Lab
+//
+//  Created by Matthew Parker on 13/7/2022.
+//
+
+import SwiftUI
+
+@main
+struct Soil_LabApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
