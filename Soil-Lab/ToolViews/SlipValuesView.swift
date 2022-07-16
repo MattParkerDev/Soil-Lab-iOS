fc91feba851cf5eb0b5d94d1533701867dafe6cd
Matthew Parker <matthewparker@hotmail.com.au>
2022-07-16 15:26:11 +1000

    Partially completed SlipValuesView, using SliderView templates

--- Soil-Lab/ToolViews/SlipValuesView.swift
+++ Soil-Lab/ToolViews/SlipValuesView.swift
@@ -9,7 +9,21 @@ import SwiftUI
 
 struct SlipValuesView: View {
     var body: some View {
-        Text("Slip Values")
+        VStack {
+            Spacer()
+            Text("Slider 55")
+                .font(.headline)
+            SliderView(surface: "P400 Sandpaper", value: "115±10", range: "105 - 125")
+            SliderView(surface: "Lapping Paper", value: "55±6", range: "49 - 61")
+            Spacer()
+            Text("Slider 96")
+                .font(.headline)
+            SliderView(surface: "P400 Sandpaper", value: "85±10", range: "75 - 95")
+            SliderView(surface: "Lapping Paper", value: "61±3", range: "58 - 64")
+            Spacer()
+        }
+        .navigationTitle("Slip Values")
+        .navigationBarTitleDisplayMode(.large)
     }
 }
 
---
+++ Soil-Lab/ViewTemplates/SliderView.swift
@@ -0,0 +1,48 @@
+//
+//  SliderView.swift
+//  Soil-Lab
+//
+//  Created by Matthew Parker on 16/7/2022.
+//
+
+import SwiftUI
+
+struct SliderView: View {
+    var surface: String
+    var value: String
+    var range: String
+    var body: some View {
+        HStack(spacing: 0) {
+            Text(surface)
+                .foregroundColor(.white)
+                .padding(10)
+                .background(
+                    RoundedRectangle(cornerRadius: 10)
+                        .foregroundColor(.blue)
+                )
+            Text(value)
+                .padding(.vertical, 10)
+                .padding(.horizontal, 15)
+            Rectangle()
+                .frame(width: 3, height: 40)
+                .foregroundColor(.blue)
+            Text(range)
+                .padding(.vertical, 10)
+                .padding(.horizontal, 15)
+        }
+        .background(
+            RoundedRectangle(cornerRadius: 10)
+                .foregroundColor(.clear)
+                .background(
+                    RoundedRectangle(cornerRadius: 10)
+                        .stroke(Color.blue, lineWidth: 3)
+                )
+        )
+    }
+}
+
+struct SliderView_Previews: PreviewProvider {
+    static var previews: some View {
+        SliderView(surface: "Lapping Paper", value: "55+-10", range: "45 - 65")
+    }
+}
